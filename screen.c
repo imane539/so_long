96ec04c8776aa58c6b6f2c01d33728ad16b7fea8
Imane El Fouhiri <iel-fouh@c4r8p6.1337.ma>
2025-03-01 14:43:18 +0000

    idk

--- screen.c
+++ screen.c
@@ -3,8 +3,14 @@ void screen()
 {
     void	*mlx;
 	void	*mlx_win;
+    void *test;
     mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+     int width = 40;
+    int height = 40;
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Detective Conan");
+    test = mlx_xpm_to_image(mlx, "../texture/wall.bng",&width, &height);
+
     (void)mlx_win;
 	mlx_loop(mlx);
+    
 }
