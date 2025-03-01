c19a6cc26fefba2e3b28f3662f2fe9c0f983636f
Imane El Fouhiri <iel-fouh@c4r4p3.1337.ma>
2025-03-01 15:26:20 +0000

    wwwwwww

--- screen.c
+++ screen.c
@@ -1,16 +1,26 @@
 #include "so_long.h"
+#include <fcntl.h>
 void screen()
 {
     void	*mlx;
 	void	*mlx_win;
-    void *test;
+    	void	*img;
+	char	*relative_path = "textures/wall.xpm";
+	// int		img_width;
+	// int		img_height; 
+    // void *test;
     mlx = mlx_init();
-     int width = 40;
-    int height = 40;
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Detective Conan");
-    test = mlx_xpm_to_image(mlx, "../texture/wall.bng",&width, &height);
+   
+	mlx_win = mlx_new_window(mlx, 500, 500, "Detective Conan");
+    img = mlx_xpm_file_to_image(mlx, relative_path, &(int){0}, &(int){0});
+    if (open(relative_path, O_RDONLY) == -1)
+        printf("hi\n");
+    if (!img)
+        printf("hello\n");
+    mlx_put_image_to_window(mlx,  mlx_win, img, 0, 0);
+    // (void)mlx_win;
+    // (void)img;
 
-    (void)mlx_win;
 	mlx_loop(mlx);
     
 }
